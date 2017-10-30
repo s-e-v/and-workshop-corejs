39dc0cb5d604f6e5a50ecaab9c1ebaa0fbfff570
s-e-v <severino.gurashi@gmail.com>
2017-10-30 13:56:18 +0000

    Tests passing..!

--- 05_classes.test.js
+++ 05_classes.test.js
@@ -4,24 +4,21 @@
 
 */
 
-function Animal() { }
+class Animal {
+  speak() {
+    return 'meow';
+  }
 
-Animal.prototype.speak = function() {
-  return "meow";
+  static eat() {
+    return 'nom nom nom';
+  }
 }
-
-Animal.eat = function() {
-  return 'nom nom nom';
-}
-
-const Cat = new Animal();
 
 test('Make cat meow', () => {
+  const Cat = new Animal();
 
-    const Cat = new Animal();
-
-    expect(Cat.speak()).toBe('meow');
-    expect(Animal.eat()).toBe('nom nom nom');
+  expect(Cat.speak()).toBe('meow');
+  expect(Animal.eat()).toBe('nom nom nom');
 });
 
 /*
@@ -32,10 +29,14 @@ test('Make cat meow', () => {
 
 */
 
+class Kitten extends Animal {
+  speak() {
+    return 'kitten meow';
+  }
+}
+
 test('Hear the kitten meow', () => {
+  const Kitty = new Kitten();
 
-    const Kitty = new Kitten();
-
-    expect(Kitty.speak()).toBe('kitten meow');
+  expect(Kitty.speak()).toBe('kitten meow');
 });
-
