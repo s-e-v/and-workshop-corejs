9520cfdfb09d78a938ce3ab910bac739bce7d1d9
s-e-v <severino.gurashi@gmail.com>
2017-10-30 10:31:26 +0000

    Made tests pass

--- 01_variables.test.js
+++ 01_variables.test.js
@@ -1,50 +1,44 @@
 test('VAR', () => {
   var x = 5;
-  var x = 6;
   expect(x).toBe(5);
 });
 
 test('LET and VAR', () => {
-  var x = 6;
   let x = 5;
   expect(x).toBe(5);
 });
 
 test('LET', () => {
-  let x = 5;
   let x = 6;
   expect(x).toBe(6);
 });
 
 test('LET', () => {
-  let x = 5;
-
   function foo() {
     let x = 20;
 
     return x;
   }
-
+  let x = foo();
   expect(x).toBe(20);
 });
 
 test('CONST - scalar values', () => {
   const x = 5;
-  x = 'foo';
   expect(x).toBe(5);
 });
 
 test('CONST - assignment', () => {
-  const x;
-  x = 5
+  const x = 5;
   expect(x).toBe(5);
 });
 
 test('CONST - objects', () => {
   const person = {
-    "name": "Linus",
-    "age": 42
+    name: 'Linus',
+    lastname: 'torvalds',
+    age: 42
   };
-  
+
   expect(person.lastname).toBe('torvalds');
 });
